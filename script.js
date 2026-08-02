72dd6295a87ab43315eb44a6fb18554a1787533f
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-08-02 21:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str]);
+	return (blob.size); 
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
-alert(byteSize(str));
+alert(byteSize(str))
+
